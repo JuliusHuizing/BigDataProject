6ed4ff837290ae7c461ae82a129c13273bb4db76
Julius <jbhuizing@gmail.com>
2024-03-11 14:42:26 +0100

    Add enum for loading train, test, and val data.

--- DataLoader.py
+++ DataLoader.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+from enum import Enum
+
 
 # os.environ['PYSPARK_SUBMIT_ARGS'] = "--master mymaster --total-executor 2 --conf 'spark.driver.extraJavaOptions=-Dhttp.proxyHost=proxy.mycorp.com-Dhttp.proxyPort=1234' -Dhttp.nonProxyHosts=localhost|.mycorp.com|127.0.0.1 -Dhttps.proxyHost=proxy.mycorp.com -Dhttps.proxyPort=1234 -Dhttps.nonProxyHosts=localhost|.mycorp.com|127.0.0.1 pyspark-shell"
 from pyspark.sql import SparkSession
@@ -12,15 +14,45 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType, 
 import os
 
 
+# enum for train and test data
+
+ 
+class DataSet(Enum):
+    TRAIN = 1
+    VALIDATION = 2
+    TEST = 3
+    
+    def get_file_paths(self) -> list:
+        base_path = 'data/'
+        result = []
+        if self == DataSet.TRAIN:
+            # append all files that start with 'train'
+            for file in os.listdir(base_path):
+                if file.startswith('train'):
+                    result.append(base_path + file)
+        elif self == DataSet.VALIDATION:
+            result.append(f'{base_path}validation_hidden.csv')
+        elif self == DataSet.TEST:
+            result.append(f'{base_path}test_hidden.csv')
+        else:
+            print("invalid data type")
+            return []
+        
+        return result
+        
+         
+        
+
 class DataLoader:
-    def __init__(self):
+    def __init__(self, data_set: DataSet):
         # Create a SparkSession
         self.spark = SparkSession.builder \
             .appName("ProductReviews") \
             .getOrCreate()
+        self.data_set = data_set
         self._create_schema()
-        self._load_train_data()
-        self._clean_train_data()
+        self._load_data()
+        self._clean_data()
             
     def _create_schema(self):
         self.schema = StructType([
@@ -38,44 +70,38 @@ class DataLoader:
             StructField("label", StringType(), True)
         ])
         
-    def _load_train_data(self):
+    def _load_data(self):
         # List of file paths for training data
-        data_folder = 'data'
-        data_files = 8
-
-        train_files = []
-
-        for file in range(1, data_files+1):
-            train_files.append(f'{data_folder}/train-{file}.csv')
+        file_names = self.data_set.get_file_paths()
 
         # Read each CSV file into a PySpark DataFrame
-        dfs = [self.spark.read.csv(file, header=True, schema=self.schema) for file in train_files]
+        dfs = [self.spark.read.csv(file, header=True, schema=self.schema) for file in file_names]
 
         # Merge all DataFrames into one
         merged_df = reduce(DataFrame.unionByName, dfs)
-        self.train_df = merged_df
+        self.df = merged_df
         
-    def _clean_train_data(self):
+    def _clean_data(self):
         self._remove_duplicate_rows()
         self._drop_dirty_data()
         
     def _remove_duplicate_rows(self):
         # Delete duplicate rows
-        duplicate_rows = self.train_df.count() - self.train_df.dropDuplicates().count()
-        self.train_df.dropDuplicates()
+        duplicate_rows = self.df.count() - self.df.dropDuplicates().count()
+        self.df.dropDuplicates()
         print(f"Removed {duplicate_rows} duplicate rows.")
         
     def _drop_dirty_data(self):
         # Ensuring categorical values can only have correct values
-        self.train_df = self.train_df.withColumn("vine", when(self.train_df["vine"].isin('Y', 'N'), self.train_df["vine"]).otherwise(None))
-        self.train_df = self.train_df.withColumn("verified_purchase", when(self.train_df["verified_purchase"].isin('Y', 'N'), self.train_df["verified_purchase"]).otherwise(None))
-        self.train_df = self.train_df.withColumn("label", when(self.train_df["label"].isin('True', 'False'), self.train_df["label"]).otherwise(None))
+        self.df = self.df.withColumn("vine", when(self.df["vine"].isin('Y', 'N'), self.df["vine"]).otherwise(None))
+        self.df = self.df.withColumn("verified_purchase", when(self.df["verified_purchase"].isin('Y', 'N'), self.df["verified_purchase"]).otherwise(None))
+        self.df = self.df.withColumn("label", when(self.df["label"].isin('True', 'False'), self.df["label"]).otherwise(None))
         
         # Select columns with categorical values
         columns_to_check = ["vine", "verified_purchase", "label"]
 
         # Remove rows where specified columns contain null or NaN values
-        self.train_df = self.train_df.filter(
+        self.df = self.df.filter(
             (col(columns_to_check[0]).isNotNull()) &
             (col(columns_to_check[1]).isNotNull()) &
             (col(columns_to_check[2]).isNotNull())
