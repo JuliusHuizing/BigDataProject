f82f765585dc2dd04746c7df64f68d6f0a8c87fa
Julius <jbhuizing@gmail.com>
2024-03-19 14:25:52 +0100

    auto open index.html when data check fails.

--- Pipelines/Check/GXDataQualityCheckModule.py
+++ Pipelines/Check/GXDataQualityCheckModule.py
@@ -5,16 +5,17 @@ from pyspark.sql import SparkSession
 from great_expectations.dataset.sparkdf_dataset import SparkDFDataset
 from great_expectations.data_context import DataContext
 import great_expectations as gx
-
+import logging
+import webbrowser
+import os
 class GXDataQualityCheckModule(DataQualityCheckModule):
         
     def __init__(self, expectation_suite_name: str, continue_after_failure: bool):
         self.context = DataContext("gx/")
         self.expectation_suite_name = expectation_suite_name
         self.continue_on_failure = continue_after_failure
-        datasource = self.context.sources.add_or_update_spark("my_spark_datasource")
-        # _ = datasource.delete_asset("meh")
-        self.data_asset = datasource.add_dataframe_asset("meh")
+        datasource = self.context.sources.add_or_update_spark("spark_data_source")
+        self.data_asset = datasource.add_dataframe_asset("asset")
 
     def _check(self, data: DataFrame) -> bool:
         batch_request = self.data_asset.build_batch_request(dataframe=data)
@@ -32,7 +33,18 @@ class GXDataQualityCheckModule(DataQualityCheckModule):
         
 
     def process(self, data: DataFrame) -> DataFrame:
-        if not self._check(data) and not self.continue_on_failure:
-            raise ValueError("Data quality check failed")
+        if not self._check(data):
+            if self.continue_on_failure:
+                logging.warning("🟠 Data quality check failed, but continuing according to config.")
+                return data
+            else:
+                # FIXME: this require absolute path...
+                # url = 'file:///../../gx/uncommited/data_docs/index.html'
+                dirname = os.path.dirname(__file__)
+                url = 'file://' + os.path.join(dirname, '../../gx/uncommitted/data_docs/local_site/index.html')
+                webbrowser.open(url, new=2)
+
+                raise ValueError("Data quality check failed")   
+            
         else:
             return data
