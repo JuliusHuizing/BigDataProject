2e83194ea94558e9a4a587b68673d8c35ee84797
Roderick Majoor <roderickmajoor@gmail.com>
2024-03-22 09:49:34 +0100

    Added csv fix to dataloader

--- modules/Collect/DataLoader.py
+++ modules/Collect/DataLoader.py
@@ -26,6 +26,7 @@ from pyspark.ml.feature import Imputer, StringIndexer, OneHotEncoder
 from pyspark.ml import Pipeline
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
 import os
+import shutil
 
 import logging
 
@@ -43,6 +44,7 @@ class DataLoader:
         self.data_dir = data_dir
         self.n = n
         self.create_schema()
+        self.fix_csv()
         
     def collect_data(self) -> list[DataFrame]:
         # List of file paths for training data
@@ -84,6 +86,27 @@ class DataLoader:
 
         self.schema = StructType(schema_fields)
         # logging.info(f"Schema created: {self.schema}")
+        
+    def fix_csv(self):
+        # Iterate over all CSV files in the directory
+        for filename in os.listdir(self.data_dir):
+            if filename.endswith(".csv"):
+                # Define the path to the original CSV file
+                original_file = os.path.join(self.data_dir, filename)
+
+                # Create a temporary file to store the preprocessed content
+                temp_file = original_file + ".temp"
+
+                # Read the original CSV file line by line, preprocess each line, and write to the temporary file
+                with open(original_file, 'r', encoding='utf-8') as input_file:
+                    with open(temp_file, 'w', encoding='utf-8') as output_file:
+                        for line in input_file:
+                            # Remove backslashes from the line
+                            preprocessed_line = line.replace('\\""', '')
+                            output_file.write(preprocessed_line)
+
+                # Replace the original file with the temporary file
+                shutil.move(temp_file, original_file)
 
 
 
